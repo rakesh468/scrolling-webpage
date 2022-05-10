869474415a7318e47bc3464f5558aad96022d814
rakesh468 <rakeshkumarg2000@gmail.com>
2022-05-10 10:33:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   const fetchcomments = async () => {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=20`
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=20` //using json placeholder api to render the data//
     );
     const data = await response.json();
     return data;
